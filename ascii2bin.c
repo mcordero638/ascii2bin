52d9564427e75c111f6e463416d5103a698560ed
Marsie Cordero <marsiecordero@gmail.com>
2021-03-04 15:06:08 -0800

    Update ascii2bin.c

--- ascii2bin.c
+++ ascii2bin.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
-#include <unistd.h> 
+#include <unistd.h>
 
 int main()
 {
-    int offset = 32;  // ascii value of '0'
-    int number = 0;
-    char ascii_value;
-    
-    int retval = read(0, &ascii_value, 1);
-    while ((retval == 1) && (ascii_value != '\n')) {
-        int digit = ascii_value - offset;
-        if ((digit == 1) || (digit == 0))
-            number = (number << 1) + digit;  
-        retval = read(0, &ascii_value, 1);
-    }    
-    printf("%d\n", number);
+int offset = 48; // ascii value of '0'
+int number = 0;
+char ascii_value;
+int digit;
+int retval;
 
-    return 0;
+retval = read(0, &ascii_value, 1);
+while (retval==1 && ascii_value != '\n') {
+switch (ascii_value) {
+case ('0'): //if ascii_value is 0 or 1 and number is not too big, break out of switch
+case ('1'):
+if (!(number & 0x80000000)) break;
+default: return 0; //return 0 upon failure
+}
+digit = ascii_value - offset;
+number = (number << 1) + digit;
+retval = read(0, &ascii_value, 1);
+}
+printf("%u\n", number);
+return 1;
 }
